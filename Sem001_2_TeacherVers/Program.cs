75c46f8ee6f9c48e57ecad3fe5c78905621cb79f
Lizaveta <drago719@mail.ru>
2024-11-21 14:46:48 +0300

    Teacher version

---
+++ Sem001_2_TeacherVers/Program.cs
@@ -0,0 +1,17 @@
+﻿int i = int.Parse(Console.ReadLine());
+
+if (int.TryParse(Console.ReadLine(), out int j))
+{
+    if (i == Math.Pow(j, 2))
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
+else
+{
+    Console.WriteLine("It's not a number");
+}
