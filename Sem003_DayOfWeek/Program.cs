667585c40a435460158ea857eb552a58c7db61ad
Lizaveta <drago719@mail.ru>
2024-11-21 14:36:22 +0300

    Sem003_DayOfWeek project

---
+++ Sem003_DayOfWeek/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите число от 1 до 7 ");
+
+int day = int.Parse(Console.ReadLine());
+
+string s = "Не день недели!";  //можно оставить пустые кавычки, тогда выведется пустота после цифры
+
+if (day == 1)
+{
+    s = "Понедельник";
+}
+else if (day == 2)
+{
+    s = "Вторник";
+}
+else if (day == 3)
+{
+    s = "Среда";
+}
+else if (day == 4)
+{
+    s = "Четверг";
+}
+else if (day == 5)
+{
+    s = "Пятница";
+}
+else if (day == 6)
+{
+    s = "Суббота";
+}
+else if (day == 7)
+{
+    s = "Воскресенье";
+}
+Console.WriteLine(s);
