3f504f04faef6425b5b404325bf9375cae1a67e2
Lizaveta <drago719@mail.ru>
2024-11-21 14:06:32 +0300

    First two tasks

---
+++ Sem001/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число А");
+
+int numberA = int.Parse(Console.ReadLine());
+
+double result = Math.Pow(numberA, 2);
+string s = $"квадрат от числа {numberA} равен {result}";
+Console.WriteLine(s);
