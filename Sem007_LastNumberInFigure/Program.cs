1f42daab1ba7a78d2ca4ec341b43a0e10b53f827
Lizaveta <drago719@mail.ru>
2024-11-22 16:35:18 +0300

    Sem007_LastNumberInFigure project

---
+++ Sem007_LastNumberInFigure/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите трёхзначное число: ");
+if (int.TryParse(Console.ReadLine(), out int n))
+{
+    if (n >= 100 && n < 1000)
+    {
+        int x = (n % 10);
+        Console.WriteLine($"Последняя цифра вашего числа {x}");
+    }
+    else
+    {
+        System.Console.WriteLine("Вы ввели не трёхзначное число!");
+    }
+}
+else
+{
+    System.Console.WriteLine("Вы ввели не число!");
+}
