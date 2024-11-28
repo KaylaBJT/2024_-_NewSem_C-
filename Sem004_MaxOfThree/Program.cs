dd8c15d97c4a29eafa6406a755f297a5837cc1d2
Lizaveta <drago719@mail.ru>
2024-11-28 12:36:32 +0300

    Sem004_MaxOfThree project

---
+++ Sem004_MaxOfThree/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число А");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число С");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+;
+
+Console.Write("max = ");
+Console.WriteLine(max);
