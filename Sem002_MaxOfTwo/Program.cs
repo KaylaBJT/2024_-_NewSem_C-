b6a0c6a2a6c152c3277973aec7765bf039da5ebf
Lizaveta <drago719@mail.ru>
2024-11-28 12:22:51 +0300

    Sem002 project

---
+++ Sem002_MaxOfTwo/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число А");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B");
+int numberB = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+;
+
+Console.Write("max = ");
+Console.WriteLine(max);
