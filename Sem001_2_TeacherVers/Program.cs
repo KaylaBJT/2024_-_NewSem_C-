5c256258e3b520a9dce31071e1272b3004847957
Lizaveta <drago719@mail.ru>
2024-12-02 14:01:07 +0300

    Sem001_2_TeacherVers project renew

--- Sem001_2_TeacherVers/Program.cs
+++ Sem001_2_TeacherVers/Program.cs
@@ -1,4 +1,9 @@
-﻿int i = int.Parse(Console.ReadLine());
+﻿//Напишите программу, которая на вход принимает дв ачисла и проверяет, является ли первое число квадратом второго
+//a=25, b=5 -> да
+//a=2, b=10 -> нет
+//a=9, b=-3 -> да
+//a=-3, b=9 -> нет
+int i = int.Parse(Console.ReadLine());
 
 if (int.TryParse(Console.ReadLine(), out int j))
 {
