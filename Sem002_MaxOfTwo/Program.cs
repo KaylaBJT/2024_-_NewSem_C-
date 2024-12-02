e805e441e16ac0e9649834e65a326e17d60424e6
Lizaveta <drago719@mail.ru>
2024-12-02 13:54:54 +0300

    Sem002_MaxOfTwo project renew

--- Sem002_MaxOfTwo/Program.cs
+++ Sem002_MaxOfTwo/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.WriteLine("Введите число А");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+Console.WriteLine("Введите число А");
 int numberA = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Введите число B");
