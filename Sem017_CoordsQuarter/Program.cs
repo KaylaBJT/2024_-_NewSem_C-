d383f42a9ec7e2abd8e5528f05eb8a842e5cde72
Lizaveta <drago719@mail.ru>
2024-12-19 01:22:51 +0300

    Sem017_CoordsQuarter project

---
+++ Sem017_CoordsQuarter/Program.cs
@@ -0,0 +1,32 @@
+﻿//напишите программу, которая на вход принимает координаты точки (X и Y), причём X не равно 0, и Y не равно 0,
+//и выдаёт номер четверти плоскости, в которой находится эта точка
+Console.Write("Input x: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Input y: ");
+int y = int.Parse(Console.ReadLine());
+
+int quarter = 0;
+
+if (x > 0)
+{
+    if (y > 0)
+    {
+        quarter = 1;
+    }
+    else
+    {
+        quarter = 4;
+    }
+}
+else
+{
+    if (y > 0)
+    {
+        quarter = 2;
+    }
+    else
+    {
+        quarter = 3;
+    }
+}
+Console.WriteLine($"(X {x}, Y {y}) is {quarter} quarter!");
