fc3c7840431870d964308b0ef412dacb0b671f3e
Lizaveta <drago719@mail.ru>
2024-12-19 15:22:12 +0300

    Sem018_CoordWidth project

--- Sem018_CoordWidth/Program.cs
+++ Sem018_CoordWidth/Program.cs
@@ -1,4 +1,42 @@
 ﻿//Напишите программу, которая по заданному номеру четверти показывет диапазон вохможных координат
 //точек в этой четверти (X и Y)
-Console.WriteLine(int.MaxValue);
-Console.WriteLine(int.MinValue);
+Console.WriteLine("Введите номер четверти: ");
+int quarter = int.Parse(Console.ReadLine());
+int x_min = 0;
+int x_max = 0;
+int y_min = 0;
+int y_max = 0;
+
+if (quarter == 1)
+{
+    x_min = 1;
+    x_max = int.MaxValue;
+    y_min = 1;
+    y_max = int.MaxValue;
+}
+
+if (quarter == 2)
+{
+    x_min = int.MinValue;
+    x_max = -1;
+    y_min = 1;
+    y_max = int.MaxValue;
+}
+
+if (quarter == 3)
+{
+    x_min = int.MinValue;
+    x_max = -1;
+    y_min = -1;
+    y_max = int.MinValue;
+}
+
+if (quarter == 4)
+{
+    x_min = 1;
+    x_max = int.MaxValue;
+    y_min = -1;
+    y_max = int.MinValue;
+}
+
+Console.WriteLine($"Диапазон возможных координат: x:({x_min}, {x_max}), y:({y_min}, {y_max})");
